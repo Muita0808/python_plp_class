d1af523c7a409f45ed9fd7a5f449b4fd9a43d5ad
Muita0808 <magdalinemuita@mail.com>
2024-11-09 19:24:06 +0300

    week1

---
+++ user_input.py
@@ -0,0 +1,12 @@
+#Use the input() function to ask the user for their name and store it in a variable called "name".
+name = input('What is your name: ')
+age = input('How old are you: ')
+print(f'Hey you, my name is {name}, and i"m {age} yrs.')
+
+
+
+
+#Use the input() function to ask the user for their age and store it in a variable called "age".
+#Use the input() function to ask the user for their location and store it in a variable called "location".
+#Print out a personalized message using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
+#Save and run the program to see the output.
