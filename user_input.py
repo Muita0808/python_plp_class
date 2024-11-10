12261d8fa56eb05ccfc2603dc71a8de13960db15
Muita0808 <magdalinemuita@mail.com>
2024-11-10 18:17:47 +0300

    week2

--- user_input.py
+++ user_input.py
@@ -10,3 +10,25 @@ print(f'Hey you, my name is {name}, and i"m {age} yrs.')
 #Use the input() function to ask the user for their location and store it in a variable called "location".
 #Print out a personalized message using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
 #Save and run the program to see the output.
+
+#Write a program that accepts user input to create a list of integers. Then, compute the sum of all the integers in the list.
+inter = input[3, 5, 6, 7, 9 ]
+print(sum.inter)
+
+#Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
+favorite_books = ("Joyride", "The Hobbit", "Faithful", "Great Gatsby", "Joker")
+for book in favorite_books:
+    print(book)
+
+#Write a program that uses a dictionary to store information about a person, such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary. Then, print the dictionary to the console.
+# Create an empty dictionary to store the person's information
+person_info = {}
+person_info['name'] = input("Enter your name: ")
+person_info['age'] = input("Enter your age: ")
+person_info['favorite color'] = input("Enter your favorite color: ")
+print(person_info)
+
+
+
+#Write a program that accepts user input to create two sets of integers. Then, create a new set that contains only the elements that are common to both sets.
+#Create a program that stores a list of words. Then, use list comprehension to create a new list that contains only the words that have an odd number of characters.
